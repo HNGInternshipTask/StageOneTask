53effcbeae7e4420b27ace719d0f92b5c1a354be
Adeshina Qudus <qudusa55@gmail.com>
2024-07-02 11:18:00 +0100

    first commit

---
+++ src/test/java/com/semicolon/StageOneTask/StageOneTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.StageOneTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageOneTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
